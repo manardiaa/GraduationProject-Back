fb69cfa20efa8d26558b4c9fcd35f12e3625fd5b
manar diaa <manardiaa98@gmail.com>
2021-05-27 22:03:22 +0200

    Repo edit

--- BL/Repositories/lessonContentRepository.cs
+++ BL/Repositories/lessonContentRepository.cs
@@ -1,30 +1,57 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using BL.Bases;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL.Models
+namespace BL.Repositories
 {
-    public class lessonContent
-    {
-        [Key]
-        public int Id { get; set; }
-        public int VideoLinkId { get; set; }
-        public int QuestionGroupId { get; set; }
-        public int LessonId { get; set; }
-        public string Description { get; set; }
-        public string Type { get; set; }
-        public string Header { get; set; }
-        public string Title { get; set; }
-        [ForeignKey("QuestionGroupId")]
-        public virtual QuestionGroup QustionGroup { get; set; }
-        [ForeignKey("VideoLinkId")]
-        public virtual CourseVideos VideoLink { get; set; }
-        [ForeignKey("LessonId")]
-        public virtual lesson Lesson { get; set; }
 
+
+    public class lessonContentRepository : BaseRepository<lessonContent>
+    {
+
+        private DbContext U_DbContext;
+
+        public lessonContentRepository(DbContext U_DbContext) : base(U_DbContext)
+        {
+            this.U_DbContext = U_DbContext;
+        }
+        #region CRUB
+
+        public List<lessonContent> GetAlllessonContent()
+        {
+            return GetAll().ToList();
+        }
+
+        public bool InsertlessonContent(lessonContent lessonContent)
+        {
+            return Insert(lessonContent);
+        }
+        public void UpdatelessonContent(lessonContent lessonContent)
+        {
+            Update(lessonContent);
+        }
+        public void DeletelessonContent(int id)
+        {
+            Delete(id);
+        }
+
+        public bool ChecklessonContentExists(lessonContent lessonContent)
+        {
+            return GetAny(l => l.Id == lessonContent.Id);
+        }
+        public lessonContent GetlessonContentById(int id)
+        {
+            return GetFirstOrDefault(l => l.Id == id);
+        }
+        #endregion
     }
 }
