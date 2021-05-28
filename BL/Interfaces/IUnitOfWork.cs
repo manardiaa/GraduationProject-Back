a0e228e3af75a2299f628c28d1a5515492ef3829
maifathy916 <maifathy916@gmail.com>
2021-05-27 23:16:46 -0700

    IUnitOfWork BL Commit

--- BL/Interfaces/IUnitOfWork.cs
+++ BL/Interfaces/IUnitOfWork.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using BL.Repositories;
 
 namespace BL.Interfaces
 {
-    class IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        #region Methode
+        int Commit();
+        #endregion
+        CategoryRepository category { get; }
     }
 }
