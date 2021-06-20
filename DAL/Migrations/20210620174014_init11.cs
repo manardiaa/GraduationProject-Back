48c2f6baeb49a8dad503e5abeec2a80e1503573e
AmanyAlaa704 <79397326+AmanyAlaa704@users.noreply.github.com>
2021-06-20 19:41:06 +0200

    options migration

---
+++ DAL/Migrations/20210620174014_init11.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class init11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Opt1",
+                table: "QuestionOptiones",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Opt2",
+                table: "QuestionOptiones",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Opt3",
+                table: "QuestionOptiones",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Opt4",
+                table: "QuestionOptiones",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Opt1",
+                table: "QuestionOptiones");
+
+            migrationBuilder.DropColumn(
+                name: "Opt2",
+                table: "QuestionOptiones");
+
+            migrationBuilder.DropColumn(
+                name: "Opt3",
+                table: "QuestionOptiones");
+
+            migrationBuilder.DropColumn(
+                name: "Opt4",
+                table: "QuestionOptiones");
+        }
+    }
+}
